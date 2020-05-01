de459e84b67d174a524f00f99d25d7305c9058e3
Roman Onishchenko <orm81zp@gmail.com>
2020-05-01 18:24:10 +0300

    182 lesson passed

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -19,7 +19,7 @@ userSchema.pre('save', function(next) {
     return next();
   }
 
-  bcrypt.getSalt(10, (err, salt) => {
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
